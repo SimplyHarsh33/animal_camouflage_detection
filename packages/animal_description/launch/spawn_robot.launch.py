62791d3174e0e4cd2df86bdbb1390d172744ae34
Harsh Yadav <harshyadav9410@gmail.com>
2026-09-10 00:15:50 +0530

    fix: robot position adjustment

--- packages/animal_description/launch/spawn_robot.launch.py
+++ packages/animal_description/launch/spawn_robot.launch.py
@@ -22,7 +22,7 @@ def generate_launch_description():
         Node(
             package='gazebo_ros',
             executable='spawn_entity.py',
-            arguments=['-entity', 'animal_robot', '-file', urdf],
+            arguments=['-entity', 'animal_robot', '-file', urdf, '-z', '0.1'],
             output='screen'),
         Node(
             package='robot_state_publisher',
